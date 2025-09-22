2cc87519c770fee7cfc7ff295aafabe7d8ec8810
Cedrick Embestro <cedrick13wew@gmail.com>
2025-09-22 11:16:16 +0800

    Arragement JS

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
+        if (this.scrollY > 500) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
+        $("html").css("scrollBehavior", "auto");
     });
 
     // typing text animation script
@@ -27,43 +27,43 @@ $(document).ready(function(){
         strings: ["UI/UX Designer", "IT Support"],
         typeSpeed: 100,
         backSpeed: 60,
-        loop: true
+        loop: true,
     });
 
     var typed = new Typed(".typing-2", {
         strings: ["Video Editor", "Web Designer", "IT Support"],
         typeSpeed: 100,
         backSpeed: 60,
-        loop: true
+        loop: true,
     });
 
     // owl carousel script
-    $('.carousel').owlCarousel({
+    $(".carousel").owlCarousel({
         margin: 20,
         loop: true,
         autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
-                nav: false
+                nav: false,
             },
-            600:{
+            600: {
                 items: 2,
-                nav: false
+                nav: false,
             },
-            1000:{
+            1000: {
                 items: 3,
-                nav: false
-            }
-        }
+                nav: false,
+            },
+        },
     });
-    var loader = document.querySelector(".loader")
+    var loader = document.querySelector(".loader");
 
     window.addEventListener("load", vanish);
-    
+
     function vanish() {
-      loader.classList.add("disppear");
+        loader.classList.add("disppear");
     }
 });
