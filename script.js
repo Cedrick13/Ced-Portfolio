ba4cd3e8be6585adf5074d9219479e1a4ab87d4d
Cedrick Embestro <cedrick13wew@gmail.com>
2025-11-14 09:18:53 +0800

    Make a Blur Navbar

--- script.js
+++ script.js
@@ -67,3 +67,14 @@ $(document).ready(function () {
         loader.classList.add("disppear");
     }
 });
+
+// Blur navbar on scroll
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
