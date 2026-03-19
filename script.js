4d4dd7ae1c19572de6dc84e905203add18dcb825
Cedrick Embestro <cedrick13wew@gmail.com>
2026-03-19 11:59:25 +0800

    Add Darkmode

--- script.js
+++ script.js
@@ -335,3 +335,25 @@ menuBtn.addEventListener("click", () => {
         : '<i class="fas fa-bars"></i>';
 });
 
+// Dark mode toggle
+const toggle = document.getElementById("theme-toggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    toggle.textContent = "☀️";
+}
+
+// Toggle click
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+        toggle.textContent = "☀️";
+    } else {
+        localStorage.setItem("theme", "light");
+        toggle.textContent = "🌙";
+    }
+});
+
